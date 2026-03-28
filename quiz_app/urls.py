e3e287581d626737d162f696988862a2331240cb
sujit-prog <148777047+sujit-prog@users.noreply.github.com>
2026-03-28 17:05:55 +0530

    ui fix

--- quiz_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),  # root landing page
-    path('subjects/', include('home.urls')),  # category/subject selection page
-]
